c6fa14680825f8581a76e0e37268af09f708d4b7
Muhamad Rizki Arif Fadillah <mrafkibo@gmail.com>
2023-05-10 21:19:20 +0700

    add materi arithmatika

--- 1. Pemrograman Go-lang dari pemula sampai mahir - udemy.com/1 Golang Dasar/4, arithmatika.go
+++ 1. Pemrograman Go-lang dari pemula sampai mahir - udemy.com/1 Golang Dasar/4, arithmatika.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
+	var angka1 uint = 10
+	var angka2 uint = 100
+	fmt.Printf("%v + %v = %v\n", angka2, angka1, angka2+angka1)
+	fmt.Printf("%v + %v = %v\n", angka2, angka1, angka2-angka1)
+	fmt.Printf("%v + %v = %v\n", angka2, angka1, angka2*angka1)
+	fmt.Printf("%v + %v = %v\n", angka2, angka1, angka2/angka1)
+	angka3 := 99
+	angka4 := 30
+	fmt.Println(angka3, "+", angka4, "=", angka3+angka4)
+	fmt.Println(angka3, "-", angka4, "=", angka3-angka4)
+	fmt.Println(angka3, "*", angka4, "=", angka3*angka4)
+	fmt.Println(angka3, "/", angka4, "=", float64(angka3)/float64(angka4))
+	fmt.Println(angka3, "%", angka4, "=", angka3%angka4)
+	var koma1 = 10.6
+	var koma2 = 30.2
+	fmt.Print(koma1, "+", koma2, "=", koma1+koma2)
 
 }
